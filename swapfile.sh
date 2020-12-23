44170485816b3adeec63fede0bcf4862dac85253
guerraf-png <76536374+guerraf-png@users.noreply.github.com>
2020-12-23 18:27:09 -0300

    Update swapfile.sh

--- swapfile.sh
+++ swapfile.sh
@@ -1,6 +1,6 @@
 dd if=/dev/zero of=/extra-swap bs=1024 count=1024
 sudo swapon -s
-sudo fallocate -l 500m /swapfile
+sudo fallocate -l 1G /swapfile
 chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
