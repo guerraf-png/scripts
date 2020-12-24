92aeb9ccd7a23c5c825865d1093539fe54f0954f
guerraf-png <76536374+guerraf-png@users.noreply.github.com>
2020-12-24 12:02:16 -0300

    Update ftp1paso.sh

--- ftp1paso.sh
+++ ftp1paso.sh
@@ -22,3 +22,4 @@ mv vsftpd.conf.1 vsftpd.conf
 echo "guerra" | touch /etc/vsftpd.userlist
 sudo systemctl restart vsftpd
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/vsftpd.pem -out /etc/ssl/private/vsftpd.pem
+sudo systemctl restart vsftpd
