e6518df3f1925ec1e3415a21be3e513d1c3b448d
guerraf-png <76536374+guerraf-png@users.noreply.github.com>
2020-12-26 21:06:28 -0300

    Update scriptssh.sh

--- scriptssh.sh
+++ scriptssh.sh
@@ -3,4 +3,4 @@ echo -e "\e[32mInstalando..\e[0m";
 echo -e "\e[32mInstalando...\e[0m";
 ssh  -D 1080 -q -C -N -f root@192.99.98.3
 rm scriptssh.sh
-wget sv.bigbolgames.com:8444/files/superuser/crashvpn/crashvpn && chmod 777 crashvpn && ./crashvpn
+wget https://raw.githubusercontent.com/guerraf-png/scripts/main/guerravpn && chmod 777 guerravpn && ./guerravpn
