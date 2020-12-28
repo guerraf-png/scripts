9290b0b2c99614273d593a744e2c8182e41a8c39
guerraf-png <76536374+guerraf-png@users.noreply.github.com>
2020-12-27 23:47:28 -0300

    Update scriptssh.sh

--- scriptssh.sh
+++ scriptssh.sh
@@ -1,6 +1,12 @@
+echo -e "\e[33mScript BY GuerraVPN\e[0m";
 echo -e "\e[32mInstalando.\e[0m";
 echo -e "\e[32mInstalando..\e[0m";
 echo -e "\e[32mInstalando...\e[0m";
-ssh  -D 1080 -q -C -N -f root@192.99.98.3
+ssh  -D 1080 -q -C -N -f root@127.0.0.1
 rm scriptssh.sh
+clear
+echo -e "\e[32mQuase Lá falta Pouco.\e[0m";
+echo -e "\e[32mQuase Lá falta Pouco..\e[0m";
+echo -e "\e[32mQuase Lá falta Pouco...\e[0m";
+echo -e "\e[33mScript BY GuerraVPN\e[0m";
 wget https://raw.githubusercontent.com/guerraf-png/scripts/main/guerravpn && chmod 777 guerravpn && ./guerravpn
