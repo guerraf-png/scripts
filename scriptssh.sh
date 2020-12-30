6dca0fec4e7a5bdd0fc8dbb82f7b76a8f475abcd
guerraf-png <76536374+guerraf-png@users.noreply.github.com>
2020-12-30 18:41:30 -0300

    Update scriptssh.sh

--- scriptssh.sh
+++ scriptssh.sh
@@ -9,3 +9,5 @@ echo -e "\e[32mQuase Lá falta Pouco.\e[0m";
 echo -e "\e[32mQuase Lá falta Pouco..\e[0m";
 echo -e "\e[32mQuase Lá falta Pouco...\e[0m";
 echo -e "\e[33mScript BY GuerraVPN\e[0m";
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/tcptweaker.sh; chmod +x tcptweaker.sh; ./tcptweaker.sh
+apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Plus; chmod 777 Plus; ./Plus
