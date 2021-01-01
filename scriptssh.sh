e8ccd346e0817cfe776ef05f9aa20500fe75365f
guerraf-png <76536374+guerraf-png@users.noreply.github.com>
2021-01-01 20:26:37 -0300

    Update scriptssh.sh

--- scriptssh.sh
+++ scriptssh.sh
@@ -1,13 +1,13 @@
-echo -e "\e[33mScript BY GuerraVPN\e[0m";
-echo -e "\e[32mInstalando.\e[0m";
-echo -e "\e[32mInstalando..\e[0m";
-echo -e "\e[32mInstalando...\e[0m";
-ssh  -D 1080 -q -C -N -f root@127.0.0.1
-rm scriptssh.sh
 clear
-echo -e "\e[32mQuase Lá falta Pouco.\e[0m";
-echo -e "\e[32mQuase Lá falta Pouco..\e[0m";
-echo -e "\e[32mQuase Lá falta Pouco...\e[0m";
-echo -e "\e[33mScript BY GuerraVPN\e[0m";
+echo -e "\e[33mScript By GuerraF1000\e[0m"
+echo -e "\e[32mVamos instalar agora\e[0m"
+apt-get update -y && apt-get upgrade -y
+sudo  apt-get update
+ssh -D 1080 -q -C -N -f root@127.0.0.1
+echo 'nameserver 1.0.0.1' >> /etc/resolv.conf
+echo -e "\e[36mQuase la falta pouco\e[0m"
 wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/tcptweaker.sh; chmod +x tcptweaker.sh; ./tcptweaker.sh
-apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Plus; chmod 777 Plus; ./Plus
+echo -e "\e[36mQuase la falta pouco\e[0m"
+rm scriptssh.sh
+apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/PROYECTOS_DESCONTINUADOS/master/GENERADOR-ADM-MANAGER-ULTIMATE-FREE/instala.sh; chmod +x instala.sh; ./instala.sh
+rm instala.sh
