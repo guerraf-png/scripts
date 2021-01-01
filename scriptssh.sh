dac28b7f02eafe58e89f477a288757a7ab9c5386
guerraf-png <76536374+guerraf-png@users.noreply.github.com>
2021-01-01 20:30:10 -0300

    Update scriptssh.sh

--- scriptssh.sh
+++ scriptssh.sh
@@ -5,6 +5,8 @@ apt-get update -y && apt-get upgrade -y
 sudo  apt-get update
 ssh -D 1080 -q -C -N -f root@127.0.0.1
 echo 'nameserver 1.0.0.1' >> /etc/resolv.conf
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/PROYECTOS_DESCONTINUADOS/master/HERRAMIENTAS/BadUDP/badudp.sh; chmod +x badudp.sh; ./badudp.sh
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/PROYECTOS_DESCONTINUADOS/master/HERRAMIENTAS/Install-SSL/sslinstall.sh ; chmod + x sslinstall.sh; ./sslinstall.sh
 echo -e "\e[36mQuase la falta pouco\e[0m"
 wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/tcptweaker.sh; chmod +x tcptweaker.sh; ./tcptweaker.sh
 echo -e "\e[36mQuase la falta pouco\e[0m"
