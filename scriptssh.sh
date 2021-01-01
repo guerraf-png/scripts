aae8a6bf6261f121dde72411775a734bcad91319
guerraf-png <76536374+guerraf-png@users.noreply.github.com>
2021-01-01 20:48:38 -0300

    Update scriptssh.sh

--- scriptssh.sh
+++ scriptssh.sh
@@ -11,5 +11,5 @@ echo -e "\e[36mQuase la falta pouco\e[0m"
 wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/tcptweaker.sh; chmod +x tcptweaker.sh; ./tcptweaker.sh
 echo -e "\e[36mQuase la falta pouco\e[0m"
 rm scriptssh.sh
-apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/PROYECTOS_DESCONTINUADOS/master/GENERADOR-ADM-MANAGER-ULTIMATE-FREE/instala.sh; chmod +x instala.sh; ./instala.sh
+apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/guerraf-png/scripts/main/guerravpn; chmod +x guerravpn; ./guerravpn
 rm instala.sh
